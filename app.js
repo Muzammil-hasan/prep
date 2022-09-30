220b89522b42b86bdbf7d885bf2cd99a667b56d5
Muzammil-hasan <muzammilsyed270300@gmail.com>
2022-09-30 14:19:35 +0530

    fix: import error

--- app.js
+++ app.js
@@ -3,22 +3,9 @@ const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 
 button.addEventListener('click', () => {
-  overlay.classList.toggle('active');
+  overlay.classList.add('active');
 });
 
 btnClose.addEventListener('click', () => {
   overlay.classList.remove('active');
 });
-
-import { useState } from 'react';
-
-export default function MyModal() {
-  let [isOpen, setIsOpen] = useState(true)
-
-  function closeModal() {
-    setIsOpen(false)
-  }
-
-  function openModal() {
-    setIsOpen(true)
-  }
